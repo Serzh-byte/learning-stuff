1c7b0fd07a8b9ca2e24b4f9fcaaac532f2039ad5
Serzh-byte <serzhtadevosyan1@gmail.com>
2025-01-07 13:09:41 +0000

    Addinf the counters

--- jv/jv/views.py
+++ jv/jv/views.py
@@ -5,3 +5,8 @@ def hello(request):
     if request.method == 'POST':
         return render(request, 'jv/hello.html')
     return render(request, 'jv/hello.html')
+
+def counter(request):
+    if request.method == 'POST':
+        return render(request, 'jv/counter.html')
+    return render(request, 'jv/counter.html')
